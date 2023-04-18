5e9f8eff1648cb939585e5f85ebacf5a16aa3632
Mantu Kumar Deka <mantukumardeka@Mantus-MacBook-Air.local>
2023-04-18 18:58:51 +0530

    Initial commit one

---
+++ src/main/scala/mkdmixture/obj.scala
@@ -0,0 +1,47 @@
+package mkdmixture
+
+object obj {
+
+
+
+  def main( args : Array[String]): Unit=
+
+    {
+
+      println("====Welcome to MKD Mixture===")
+
+
+      val a = List(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+
+      println("====Raw List=====")
+
+      println(a)
+
+      println()
+
+      println("Need to number less than 10")
+
+      val b =a.filter(  x => x  <10     )
+
+      println(b)
+
+      println()
+
+      println(" Need to print values greater than 10")
+
+      println(  a.filter(y  => y >10)      )
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
